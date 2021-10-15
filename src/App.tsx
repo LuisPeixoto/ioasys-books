cc6d209bcc85283ddbe6922c72d73737ab49212f
Luis Fernando <luispeixoto@pq.uenf.br>
2021-10-15 09:01:01 -0300

    feat: global application styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import { Text } from "react-native";
+import React from 'react';
+import { Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import theme from './Global/theme';
 
 const App: React.FC = () => {
-    return(
-        <Text>ola mundo</Text>
-    )
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text
+          style={{ fontFamily: theme.fonts.medium, color: theme.colors.pink }}
+        >
+          ola mundo
+        </Text>
+      </View>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Global/theme.ts
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    pink: '#AB2680',
+    gray: '#999999',
+    light: '#FFFFFF',
+    dark: '#333333',
+  },
+
+  fonts: {
+    light: 'Heebo-Light',
+    regular: 'Heebo-Regular',
+    medium: 'Heebo-Medium',
+  },
+};
